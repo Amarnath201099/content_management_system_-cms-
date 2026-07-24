56e3f75ca73132644037dc15658ae696056b272b
amarnath201099 <amarnath201099@gmail.com>
2026-07-24 22:25:26 +0530

    Modified cookieOptions to support deployement

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -16,7 +16,7 @@ const sendTokenResponse = (user, statusCode, res, message) => {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   };
 
   res
